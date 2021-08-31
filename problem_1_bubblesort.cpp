b8a01cce9c4a8d8e49d4431cff9a27aab20c4eae
Jianze Zhou <jnz2@georgetown.edu>
2021-08-30 21:21:33 -0400

    Update problem_1_bubblesort.cpp

--- problem_1_bubblesort.cpp
+++ problem_1_bubblesort.cpp
@@ -9,7 +9,7 @@ int main() {
 		cin >> a[i];
 	}
 	// bubble sort, keep comparing neighbors and swap if wrong-ordered
-	for (int i = 1; i < n - 1; i++) {
+	for (int i = 0; i < n - 1; i++) {
 		for (int j = 1; j < n - i; j++) {
 			if (a[j - 1] > a[j]) {
 				int temp = a[j];
