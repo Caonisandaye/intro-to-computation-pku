88b54446260995f96ccc5179784cf991e5322b6d
Jianze Zhou <jnz2@georgetown.edu>
2021-08-30 21:57:03 -0400

    Update week4_1.cpp

--- week4_1.cpp
+++ week4_1.cpp
@@ -1,26 +1,26 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    int main() {
-        int n, a[1000]; // 一共n个数，n不超过1000。a用来保存这些数
-        cin >> n;  
-        // 输入n个数  
-        for (int i = 0; i < n; i++) {
-          cin >> a[i];
-    }  
-    // 冒泡，不断比较相邻的两个数，如果顺序错了，那么就交换
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = 1; j < n - i; j++) {      
-        if (a[j - 1] > a[j]) {
-          int temp = a[j];
-          a[j] = a[j - 1];
-          a[j - 1] = temp;
-        }    
-      }  
-    }  
-    // 依次输出
-   for (int i = 0; i < n; i++) {
-     cout << a[i] << endl;  
-   }  
-   return 0;
+int main() {
+	int n, a[1000]; // n numbers in total, with n <= 1000
+	cin >> n;
+	// input n numbers
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	// bubble sort, keep comparing neighbors and swap if wrong-ordered
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 1; j < n - i; j++) {
+			if (a[j - 1] > a[j]) {
+				int temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
+			}
+		}
+	}
+	// output in order
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << endl;
+	}
+	return 0;
 }
