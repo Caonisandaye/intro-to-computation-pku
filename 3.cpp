61bc5b404ef839db0741ed3db05ddbb8c5687ae9
Jianze Zhou <jnz2@georgetown.edu>
2021-08-30 22:38:33 -0400

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+	// bubble sort
+	for (int i = 0; i < 9; i++) {
+		for (int j = 1; j < 10 - i; j++) {
+			//we decide the order by first even and odd, then value
+			bool leftIsEven = a[j - 1 ] % 2 == 0;
+			bool rightIsEven = a[j] % 2 == 0;
+			if ((leftIsEven && !rightIsEven) ||
+				(leftIsEven == rightIsEven && a[j - 1] > a[j])) {
+				int temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << ' ';
+	}
+	return 0;
+}
