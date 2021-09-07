45a7e58298306c23ec832377e725c1aa16b9c9b0
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:33:24 -0400

    Update week4_2.cpp

--- week4/week4_2.cpp
+++ week4/week4_2.cpp
@@ -6,6 +6,7 @@ int main() {
   for (int i = 0; i < 10; i++) {
     cin >> a[i];
   }
+  
   //first put odd to left and even to right
   int l = 0, r = 9; //pointers both to left and right end 
   while (l <= r) {
@@ -23,6 +24,7 @@ int main() {
       a[r] = temp;
     }
   }
+  
   //bubble sort the left
   int start = 0, end = l;
   for (int i = start; i < end - 1; i++) {
@@ -34,6 +36,7 @@ int main() {
       }
     }
   }
+  
   //bubble sort the right
   start = l, end = 10;
   for (int i = start; i < end - 1; i++) {
@@ -45,6 +48,8 @@ int main() {
       }
     }
   }
+  
+  // output
   for (int i = 0; i < 10; i++) {
     cout << a[i] << ' ';
   } 
