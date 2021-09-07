91038b9cce299558ec1511c590da0b78d3bb815d
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:10:08 -0400

    Update week7_2_4.cpp

--- week7/week7_2_4.cpp
+++ week7/week7_2_4.cpp
@@ -5,7 +5,8 @@ int main() {
     int value;
     int face[6] = {100, 50, 20, 10, 5, 1};
     int need[6];
-
+    
+    // fully utilize biggest face value, calculate residual
     cin >> value;
     for (int i = 0; i < 6; i++) {
         need[i] = value / face[i];
