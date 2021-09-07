e10a62770f0115bb507724ee5f0ec436a8617c8c
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:02:42 -0400

    Update week8_2_5.cpp

--- week8/week8_2_5.cpp
+++ week8/week8_2_5.cpp
@@ -6,10 +6,14 @@ int main() {
     cin >> n;
     for (int i = 10; i <= n; i++) {
         int j = i, sumdig = 0;
+        
+        // check sum if digits
         while (j > 0) {
             sumdig += j % 10;
             j /= 10;
         }
+        
+        // check modulo
         if (i % sumdig == 0)
             cout << i << endl;
     }
