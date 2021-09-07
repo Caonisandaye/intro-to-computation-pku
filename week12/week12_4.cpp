cba3aa193f7d4f9283c8258eee358431a92ee60b
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 00:32:43 -0400

    Update week12_4.cpp

--- week12/week12_4.cpp
+++ week12/week12_4.cpp
@@ -5,7 +5,8 @@ int main() {
     int a, b;
     char o;
     cin >> a >> b >> o;
-
+    
+    // implement the logic of +-*/ and handle the exceptions
     if (o == '+')
         cout << a + b;
     else if (o == '-')
