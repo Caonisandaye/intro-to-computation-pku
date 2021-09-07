4d8733474a298598a39b23041b41b52b7ea7660f
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:23:33 -0400

    Update week6_2_1.cpp

--- week6/week6_2_1.cpp
+++ week6/week6_2_1.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main() {
     int n, x, y;
     cin >> n >> x >> y;
+    
     // apple eaten by worm
     int eaten = y / x + (y % x > 0);
     if (eaten > n)
         cout << 0 << endl;
     else
         cout << n - eaten << endl;
+    
     return 0;
 }
