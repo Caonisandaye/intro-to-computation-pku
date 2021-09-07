abc347bd73db143d420692871c1fe5bd11ae9207
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 00:45:34 -0400

    Update and rename week10_7.cpp to week10_7a.cpp

--- week10/week10_7a.cpp
+++ week10/week10_7a.cpp
@@ -8,6 +8,8 @@ int main() {
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
+        
+        // sort numbers
         for (int i = n - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (a[j] > a[j+1]) {
@@ -17,6 +19,8 @@ int main() {
                 }
             }
         }
+        
+        // output medians
         if (n % 2 == 1)
             cout << a[n/2] << endl;
         else
