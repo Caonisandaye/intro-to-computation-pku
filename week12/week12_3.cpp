bdd7e8960e2b71397efda28fd72ba6c5b08a5ee2
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 00:43:03 -0400

    Update week12_3.cpp

--- week12/week12_3.cpp
+++ week12/week12_3.cpp
@@ -11,11 +11,12 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> p[i][0] >> p[i][1];
     }
-
+    
+    // calculating euclidean distance for pairs
     double dis = 0.0;
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            double cur_dis = sqrt((p[i][1]-p[j][1])*(p[i][1]-p[j][1])+(p[i][0]-p[j][0])*(p[i][0]-p[j][0]));
+            double cur_dis = sqrt(pow(p[i][0]-p[j][0], 2) + pow(p[i][1]-p[j][1], 2));
             if (cur_dis > dis) {
                 dis = cur_dis;
             }
