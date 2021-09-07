b9b421c9b9f08221f440c7f6164743a1fb5986b7
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:21:31 -0400

    Update week6_2_5.cpp

--- week6/week6_2_5.cpp
+++ week6/week6_2_5.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main() {
     int tri, hun_bit, ten_bit, uni_bit;
     cin >> tri;
+    
     // calculate bits
     hun_bit = tri / 100;
     ten_bit = (tri % 100) / 10;
     uni_bit = tri % 10;
+    
     // output bits
     cout << hun_bit << endl;
     cout << ten_bit << endl;
