f27947181dab5a8dde7b58c2be07d0a856223462
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:05:01 -0400

    Update week8_2_4.cpp

--- week8/week8_2_4.cpp
+++ week8/week8_2_4.cpp
@@ -8,6 +8,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
+    
+    // check if there is a satisfactory pair, default false
     bool output = false;
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
@@ -15,6 +17,7 @@ int main() {
                 output = true;
         }
     }
+    
     cout << (output?"yes":"no") << endl;
 
     return 0;
