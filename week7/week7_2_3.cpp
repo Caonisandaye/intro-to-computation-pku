96174c3a6a7ed0c92a36978ffeee3b89c9dbdde2
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:16:49 -0400

    Update week7_2_3.cpp

--- week7/week7_2_3.cpp
+++ week7/week7_2_3.cpp
@@ -4,20 +4,25 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-
+    
+    // a stack to keep largest k values
     int a[m];
     for (int i = 0; i < m; i++)
         a[i] = 0;
-
+    
+    // insert new value if necessary
     for (int i = 0; i < n; i++){
         int nxt;
         cin >> nxt;
+        
+        // find the right place to insert
         int comp = 0;
         while (comp < m && nxt > a[comp])
             comp += 1;
         for (int j = 1; j < comp; j++) {
             a[j - 1] = a[j];
         }
+        
         a[comp - 1] = nxt;
     }
 
