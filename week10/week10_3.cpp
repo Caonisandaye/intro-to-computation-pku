b756b85d80f58f976aa5c550824106bdea50024c
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 00:57:41 -0400

    Update week10_3.cpp

--- week10/week10_3.cpp
+++ week10/week10_3.cpp
@@ -5,7 +5,8 @@ int main() {
     char str[80];
     cin.getline(str, 80);
     int opt_start = 0, opt_end = 0, cur_start = 0, cur_end = 0;
-
+    
+    // find the longest word by recording start and end letter, each end letter should be a space or period
     for (int i = 0; i < 80; i++) {
         if (str[i] == ' ' || str[i] == '.') {
             cur_end = i;
