3cb3ae7793e4ccf37cd0ac46e21e6ddd90d309dd
Jianze Zhou <jnz2@georgetown.edu>
2021-09-07 01:01:51 -0400

    Update week8_2_3.cpp

--- week8/week8_2_3.cpp
+++ week8/week8_2_3.cpp
@@ -5,6 +5,8 @@ int main() {
     int N, K;
     while (cin >> N >> K) {
         double save = 0, house = 200;
+        
+        // get income, check house price, then inflation
         for (int i = 1; i <= 20; i++) {
             save += N;
             if (save >= house){
@@ -13,6 +15,8 @@ int main() {
             }
             house *= (1 + K/100.0);
         }
+        
+        // cant afford in 20 years
         if (save < house)
             cout << "Impossible" << endl;
     }
